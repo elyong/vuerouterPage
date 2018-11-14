ef60093e59feb422aaf116dc9adfce56a177f2b1
raypeng <1160320939@qq.com>
2018-11-14 10:47:10 +0800

    @1.0.0

---
+++ src/components/PageName.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="page-name">
+      <div class="left-side">
+        <router-link :class="{tab:true}" v-for="name in names" :to="'/page1/'+name" tag="li">{{name}}</router-link>
+      </div>
+      <div class="content">
+        <label>名字：
+          <input :value="this.$route.params.name"/>
+        </label>
+      </div>
+    </div>
+
+
+</template>
+
+<script>
+    export default {
+        name: "PageName",
+        data(){
+          return{
+            names:['freddy','nick','mike']
+          }
+        }
+    }
+</script>
+
+<style scoped>
+  .page-name{
+    height:100vh;
+    width:100%;
+  }
+  .left-side{
+    background-color: #252525;
+    color:white;
+    width:15%;
+    height:100%;
+    padding:15px;
+    display: inline-block;
+  }
+  .left-side .tab{
+    margin:5px;
+    cursor: pointer;
+  }
+  .content{
+    display: inline-block;
+    vertical-align: top;
+    padding:20px;
+  }
+  .router-link-active{
+    color:orange;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import PageName from '@/components/PageName'
+
+Vue.use(Router)
+
+export default new Router({
+  mode:'history',
+  routes: [
+    {
+      path:'/',
+      redirect:'/page1/freddy'
+    },
+    {
+      path:'/page1/:name',
+      name:'pageName',
+      component:PageName
+    }
+  ]
+})
